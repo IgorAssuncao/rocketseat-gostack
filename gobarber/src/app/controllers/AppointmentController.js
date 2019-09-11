20d7134114e558466f5ffdce9055cbf255419a82
IgorAssuncao <igor.ormonde@gmail.com>
2019-09-11 06:02:15 -0300

    mongo init and schedule appointments

--- gobarber/src/app/controllers/AppointmentController.js
+++ gobarber/src/app/controllers/AppointmentController.js
@@ -1,9 +1,44 @@
 import * as Yup from 'yup';
+import { startOfHour, parseISO, isBefore, format } from 'date-fns';
 
 import User from '../models/User';
+import File from '../models/File';
 import Appointment from '../models/Appointment';
 
+import Notification from '../schemas/Notification';
+
 class AppointmentController {
+  async index(request, response) {
+    const { page = 1 } = request.query;
+
+    const appointments = await Appointment.findAll({
+      where: {
+        user_id: request.userId,
+        canceled_at: null,
+      },
+      order: ['date'],
+      attributes: ['id', 'date'],
+      limit: 20,
+      offset: (page - 1) * 20,
+      include: [
+        {
+          model: User,
+          as: 'provider',
+          attributes: ['id', 'name'],
+          include: [
+            {
+              model: File,
+              as: 'avatar',
+              attributes: ['id', 'path', 'url'],
+            },
+          ],
+        },
+      ],
+    });
+
+    return response.json(appointments);
+  }
+
   async store(request, response) {
     const schema = Yup.object().shape({
       provider_id: Yup.number().required(),
@@ -25,10 +60,40 @@ class AppointmentController {
         .json({ error: 'You can only create appointments with providers' });
     }
 
+    const hourStart = startOfHour(parseISO(date));
+
+    if (isBefore(hourStart, new Date())) {
+      return response
+        .status(400)
+        .json({ error: 'Past dates are not permitted' });
+    }
+
+    const appointmentAvailability = await Appointment.findOne({
+      where: {
+        provider_id,
+        canceled_at: null,
+        date: hourStart,
+      },
+    });
+
+    if (appointmentAvailability) {
+      return response
+        .status(400)
+        .json({ error: 'Appointment date is not available' });
+    }
+
     const appointment = await Appointment.create({
       user_id: request.userId,
       provider_id,
       date,
+    });
+
+    const user = await User.findByPk(request.userId);
+    const formattedDate = format(hourStart, "dd'/'MMMM'-'H:mm'h'");
+
+    await Notification.create({
+      content: `New appointment of ${user.name} to ${formattedDate}`,
+      user: provider_id,
     });
 
     return response.json(appointment);
--- gobarber/src/database/index.js
+++ gobarber/src/database/index.js
@@ -1,4 +1,5 @@
 import Sequelize from 'sequelize';
+import mongoose from 'mongoose';
 
 import User from '../app/models/User';
 import File from '../app/models/File';
@@ -11,6 +12,7 @@ const models = [User, File, Appointment];
 class Database {
   constructor() {
     this.init();
+    this.mongo();
   }
 
   init() {
@@ -19,6 +21,14 @@ class Database {
     models.map(model => model.init(this.connection));
     models.map(
       model => model.associate && model.associate(this.connection.models)
+    );
+  }
+
+  mongo() {
+    const host = process.env.NODE_ENV !== undefined ? 'mongo' : '0.0.0.0';
+    this.mongoConnection = mongoose.connect(
+      `mongodb://${host}:27017/gobarber`,
+      { useNewUrlParser: true, useFindAndModify: true }
     );
   }
 }
